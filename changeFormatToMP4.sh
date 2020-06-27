ffb58e322724dc77cdd2a8b6392424c9c4e5d7ff
Shin <fyuneru0830@hotmail.com>
2020-06-27 21:05:53 +0900

    debug

--- changeFormatToMP4.sh
+++ changeFormatToMP4.sh
@@ -6,7 +6,7 @@ echo "--------------------"
 echo -e -n "\033[0;39m"
 echo -e -n "\033[1;34mvideo file path(MacOS can drop file to this window):\033[0;39m"
 read PATH
-/usr/local/bin/ffmpeg -i $PATH
+/usr/local/bin/ffmpeg -i "$PATH"
 #echo -e -n "\033[1;33mSource video "
 #/usr/local/bin/ffprobe  -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1 "$PATH"
 echo -e -n "\033[1;34mNew video bit rate(KB/s)(Recommand same with source video bit rate):\033[0;39m"
